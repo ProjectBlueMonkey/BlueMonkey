d2ed5855bba93da51584377ae7709c392bf84a79
nuits.jp <nuits.jp@live.jp>
2016-11-02 17:07:18 +0900

    Add ReportListPage

--- client/BlueMonkey/BlueMonkey/ViewModels/ReportListPageViewModel.cs
+++ client/BlueMonkey/BlueMonkey/ViewModels/ReportListPageViewModel.cs
@@ -3,14 +3,32 @@ using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Prism.Navigation;
 
 namespace BlueMonkey.ViewModels
 {
     public class ReportListPageViewModel : BindableBase
     {
-        public ReportListPageViewModel()
-        {
+        private readonly INavigationService _navigationService;
+        /// <summary>
+        /// Add New Report Navigation Command.
+        /// </summary>
+        public DelegateCommand AddReportCommand => new DelegateCommand(AddReport);
 
+        /// <summary>
+        /// Initialize Instance
+        /// </summary>
+        /// <param name="navigationService"></param>
+        public ReportListPageViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+        }
+
+        /// <summary>
+        /// Navigation ReportPage.
+        /// </summary>
+        private void AddReport()
+        {
         }
     }
 }
