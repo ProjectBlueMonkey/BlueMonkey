7a918aa7051556cb84bc08fb6cddaaf21925691a
nuits.jp <nuits.jp@live.jp>
2016-11-02 09:30:17 +0900

    Add ExpenceService And ExpenceService.Local

---
+++ client/BlueMonkey/BlueMonkey.ExpenceServices.Local/ExpenseService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlueMonkey.Business;
+
+namespace BlueMonkey.ExpenceServices.Local
+{
+    public class ExpenseService : IExpenseService
+    {
+        public Task<IEnumerable<Expense>> GetExpenses()
+        {
+            var list = new List<Expense>();
+            for (int x = 0; x < 10; x++)
+            {
+                list.Add(new Expense() { Name = $"Expense{x}" });
+            }
+
+            return Task.FromResult(list.AsEnumerable());
+        }
+    }
+}
---
+++ client/BlueMonkey/BlueMonkey.ExpenceServices/IExpenseService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlueMonkey.Business;
+
+namespace BlueMonkey.ExpenceServices
+{
+    public interface IExpenseService
+    {
+        Task<IEnumerable<Expense>> GetExpenses();
+    }
+}
