5ac00ad9220eca5792d15eab8b7d48a951eca509
nuits.jp <nuits.jp@live.jp>
2016-11-14 23:50:45 +0900

    Update ReportListPage.
    * ListViewの追加
    * ViewModelへINavigationAwareの追加
    * IExpenseServiceのInjectionの追加
    * OnNavigatedToによるリストの初期化処理の追加

--- client/BlueMonkey/BlueMonkey/ViewModels/ReportListPageViewModel.cs
+++ client/BlueMonkey/BlueMonkey/ViewModels/ReportListPageViewModel.cs
@@ -2,14 +2,19 @@
 using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using BlueMonkey.Business;
+using BlueMonkey.ExpenceServices;
 using Prism.Navigation;
 
 namespace BlueMonkey.ViewModels
 {
-    public class ReportListPageViewModel : BindableBase
+    public class ReportListPageViewModel : BindableBase, INavigationAware
     {
         private readonly INavigationService _navigationService;
+        private readonly IExpenseService _expenseService;
+        public ObservableCollection<Report> Reports { get; } = new ObservableCollection<Report>();
         /// <summary>
         /// Add New Report Navigation Command.
         /// </summary>
@@ -19,9 +24,11 @@ namespace BlueMonkey.ViewModels
         /// Initialize Instance
         /// </summary>
         /// <param name="navigationService"></param>
-        public ReportListPageViewModel(INavigationService navigationService)
+        /// <param name="expenseService"></param>
+        public ReportListPageViewModel(INavigationService navigationService, IExpenseService expenseService)
         {
             _navigationService = navigationService;
+            _expenseService = expenseService;
         }
 
         /// <summary>
@@ -30,6 +37,19 @@ namespace BlueMonkey.ViewModels
         private void AddReport()
         {
             _navigationService.NavigateAsync("ReportPage");
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+        }
+
+        public async void OnNavigatedTo(NavigationParameters parameters)
+        {
+            Reports.Clear();
+            foreach (var report in await _expenseService.GetReportsAsync())
+            {
+                Reports.Add(report);
+            }
         }
     }
 }
