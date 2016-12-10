7540b7e71156738b856a879fd2bb9eaf5a599029
nuits.jp <nuits.jp@live.jp>
2016-12-10 17:06:22 +0900

    Implement UnitTest for ReportPageViewModel#Constructor.

---
+++ client/BlueMonkey/BlueMonkey.ViewModel.Tests/ReportPageViewModelTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlueMonkey.Model;
+using BlueMonkey.ViewModels;
+using Moq;
+using Prism.Navigation;
+using Xunit;
+
+namespace BlueMonkey.ViewModel.Tests
+{
+    public class ReportPageViewModelTest
+    {
+        [Fact]
+        public void Constructor()
+        {
+            var navigationService = new Mock<INavigationService>();
+            var editReport = new Mock<IEditReport>();
+            editReport.Setup(m => m.Name).Returns("Name");
+            editReport.Setup(m => m.Date).Returns(DateTime.Today);
+
+            var actual = new ReportPageViewModel(navigationService.Object, editReport.Object);
+
+            Assert.NotNull(actual.Name);
+            Assert.Equal("Name", actual.Name.Value);
+
+            Assert.NotNull(actual.Date);
+            Assert.Equal(DateTime.Today, actual.Date.Value);
+
+            Assert.Null(actual.Expenses);
+
+            Assert.NotNull(actual.InitializeCommand);
+            Assert.True(actual.InitializeCommand.CanExecute());
+
+            Assert.NotNull(actual.NavigateExpenseSelectionCommand);
+            Assert.True(actual.NavigateExpenseSelectionCommand.CanExecute());
+
+            Assert.NotNull(actual.SaveReportCommand);
+            Assert.True(actual.SaveReportCommand.CanExecute());
+        }
+    }
+}
