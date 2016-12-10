11c82a0e7268fd76b200b005fac910167baa4230
nuits.jp <nuits.jp@live.jp>
2016-12-10 12:46:26 +0900

    Implement UnitTest for EditReport constructor.

--- client/BlueMonkey/BlueMonkey.Model.Tests/EditReportTest.cs
+++ client/BlueMonkey/BlueMonkey.Model.Tests/EditReportTest.cs
@@ -3,11 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BlueMonkey.ExpenceServices;
+using Moq;
+using Xunit;
 
 namespace BlueMonkey.Model.Tests
 {
     public class EditReportTest
     {
+        [Fact]
+        public void Constructor()
+        {
+            var expenseService = new Mock<IExpenseService>();
+            var actual = new EditReport(expenseService.Object);
 
+            Assert.Null(actual.Name);
+
+            Assert.Equal(default(DateTime), actual.Date);
+
+            Assert.NotNull(actual.SelectableExpenses);
+            Assert.False(actual.SelectableExpenses.Any());
+        }
     }
 }
