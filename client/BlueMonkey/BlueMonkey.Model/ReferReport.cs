444e6d2a0a5e765c8fd6074b2362cad317c734f5
nuits.jp <nuits.jp@live.jp>
2016-12-11 22:39:31 +0900

    Add ReferReport classes.

---
+++ client/BlueMonkey/BlueMonkey.Model/ReferReport.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlueMonkey.Business;
+
+namespace BlueMonkey.Model
+{
+    public class ReferReport : IReferReport
+    {
+        private readonly ObservableCollection<Report> _reports = new ObservableCollection<Report>();
+        public ReadOnlyObservableCollection<Report> Reports { get; }
+        public Task Search()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
