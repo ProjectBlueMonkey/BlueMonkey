9c143656eb0da2fcf50a9ad33e036c899448ce11
nuits.jp <nuits.jp@live.jp>
2016-12-11 22:43:05 +0900

    Implement ReferReport#Constructor.

--- client/BlueMonkey/BlueMonkey.Model.Tests/ReferReportTest.cs
+++ client/BlueMonkey/BlueMonkey.Model.Tests/ReferReportTest.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BlueMonkey.ExpenceServices;
+using Moq;
+using Xunit;
 
 namespace BlueMonkey.Model.Tests
 {
     public class ReferReportTest
     {
+        [Fact]
+        public void Constructor()
+        {
+            var expenseService = new Mock<IExpenseService>();
+            var actual = new ReferReport(expenseService.Object);
+
+            Assert.NotNull(actual.Reports);
+            Assert.Equal(0, actual.Reports.Count);
+        }
     }
 }
--- client/BlueMonkey/BlueMonkey.Model/ReferReport.cs
+++ client/BlueMonkey/BlueMonkey.Model/ReferReport.cs
@@ -5,13 +5,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BlueMonkey.Business;
+using BlueMonkey.ExpenceServices;
 
 namespace BlueMonkey.Model
 {
     public class ReferReport : IReferReport
     {
+        private readonly IExpenseService _expenseService;
         private readonly ObservableCollection<Report> _reports = new ObservableCollection<Report>();
         public ReadOnlyObservableCollection<Report> Reports { get; }
+        public ReferReport(IExpenseService expenseService)
+        {
+            _expenseService = expenseService;
+            Reports = new ReadOnlyObservableCollection<Report>(_reports);
+        }
+
         public Task Search()
         {
             throw new NotImplementedException();
