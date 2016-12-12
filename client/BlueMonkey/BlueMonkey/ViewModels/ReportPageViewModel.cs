c8adde1c4a911d0434e653138ce0826ba185ba0f
nuits.jp <nuits.jp@live.jp>
2016-12-12 21:36:17 +0900

    Update to generate ReportPageViewModel#SaveReportCommand from DelegateCommand#FromAsyncHandler.

--- client/BlueMonkey/BlueMonkey/ViewModels/ReportPageViewModel.cs
+++ client/BlueMonkey/BlueMonkey/ViewModels/ReportPageViewModel.cs
@@ -81,10 +81,10 @@ namespace BlueMonkey.ViewModels
                 _navigationService.NavigateAsync("ExpenseSelectionPage");
             });
 
-            SaveReportCommand = new DelegateCommand(() =>
+            SaveReportCommand = DelegateCommand.FromAsyncHandler(async () =>
             {
-                _editReport.SaveAsync();
-                _navigationService.GoBackAsync();
+                await _editReport.SaveAsync();
+                await _navigationService.GoBackAsync();
             });
         }
 
