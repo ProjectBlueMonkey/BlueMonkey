dd97ef0c5948d1caed782135bd4607c9e23b2b71
oota.kazuki <oota.kazuki@jp.fujitsu.com>
2016-12-13 14:37:04 +0900

    add Secret

---
+++ client/BlueMonkey/BlueMonkey/Secret_sample.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueMonkey
+{
+    public static class Secret
+    {
+        public static readonly string ServerUri = "https://your_app_name.azurewebsites.net";
+    }
+}
