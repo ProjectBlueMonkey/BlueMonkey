d607937505d5bd4d5526fb12bfa06ac4b0fdf6e4
oota.kazuki <oota.kazuki@jp.fujitsu.com>
2016-12-13 14:24:01 +0900

    update project files

--- client/BlueMonkey/BlueMonkey.Droid/Resources/Resource.Designer.cs
+++ client/BlueMonkey/BlueMonkey.Droid/Resources/Resource.Designer.cs
@@ -26,6 +26,8 @@ namespace BlueMonkey.Droid
 		
 		public static void UpdateIdValues()
 		{
+			global::BlueMonkey.LoginService.Droid.Resource.String.ApplicationName = global::BlueMonkey.Droid.Resource.String.ApplicationName;
+			global::BlueMonkey.LoginService.Droid.Resource.String.Hello = global::BlueMonkey.Droid.Resource.String.Hello;
 			global::Xamarin.Forms.Platform.Android.Resource.Attribute.actionBarSize = global::BlueMonkey.Droid.Resource.Attribute.actionBarSize;
 		}
 		
@@ -2948,6 +2950,12 @@ namespace BlueMonkey.Droid
 		
 		public partial class String
 		{
+			
+			// aapt resource value: 0x7f080027
+			public const int ApplicationName = 2131230759;
+			
+			// aapt resource value: 0x7f080026
+			public const int Hello = 2131230758;
 			
 			// aapt resource value: 0x7f08000f
 			public const int abc_action_bar_home_description = 2131230735;
