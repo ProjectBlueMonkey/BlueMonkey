4f06f6fe4c7320957a2c1801a9218496df7bce32
oota.kazuki <oota.kazuki@jp.fujitsu.com>
2016-12-13 14:24:12 +0900

    implement ILoginService

--- client/BlueMonkey/BlueMonkey.LoginService.Droid/AzureLoginService.cs
+++ client/BlueMonkey/BlueMonkey.LoginService.Droid/AzureLoginService.cs
@@ -2,10 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace BlueMonkey.LoginService.Droid
 {
-    public class AzureLoginService
+    public class AzureLoginService : ILoginService
     {
+        public Task<bool> LoginAsync()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- client/BlueMonkey/BlueMonkey.LoginService.iOS/AzureLoginService.cs
+++ client/BlueMonkey/BlueMonkey.LoginService.iOS/AzureLoginService.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace BlueMonkey.LoginService.iOS
 {
-    public class AzureLoginService
+    public class AzureLoginService : ILoginService
     {
+        public Task<bool> LoginAsync()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
