59928fcf6e09983d2cbc41ba4c4c00450b4355dc
oota.kazuki <oota.kazuki@jp.fujitsu.com>
2016-12-13 15:23:07 +0900

    implement Login

--- client/BlueMonkey/BlueMonkey.LoginService.Droid/AzureLoginService.cs
+++ client/BlueMonkey/BlueMonkey.LoginService.Droid/AzureLoginService.cs
@@ -1,16 +1,26 @@
+using Microsoft.WindowsAzure.MobileServices;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace BlueMonkey.LoginService.Droid
 {
     public class AzureLoginService : ILoginService
     {
-        public Task<bool> LoginAsync()
+        private readonly IMobileServiceClient _client;
+
+        public AzureLoginService(IMobileServiceClient client)
         {
-            throw new NotImplementedException();
+            _client = client;
+        }
+
+        public async Task<bool> LoginAsync()
+        {
+            var user = await _client.LoginAsync(Forms.Context, MobileServiceAuthenticationProvider.MicrosoftAccount);
+            return user != null;
         }
     }
 }
--- client/BlueMonkey/BlueMonkey.LoginService.iOS/AzureLoginService.cs
+++ client/BlueMonkey/BlueMonkey.LoginService.iOS/AzureLoginService.cs
@@ -1,16 +1,29 @@
-﻿using System;
+﻿using Microsoft.WindowsAzure.MobileServices;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
 
 namespace BlueMonkey.LoginService.iOS
 {
     public class AzureLoginService : ILoginService
     {
-        public Task<bool> LoginAsync()
+        private readonly IMobileServiceClient _client;
+
+        public AzureLoginService(IMobileServiceClient client)
         {
-            throw new NotImplementedException();
+            _client = client;
+        }
+
+        public async Task<bool> LoginAsync()
+        {
+            var user = await _client.LoginAsync(UIApplication.SharedApplication.KeyWindow.RootViewController, 
+                MobileServiceAuthenticationProvider.MicrosoftAccount);
+            return user != null;
         }
     }
 }
