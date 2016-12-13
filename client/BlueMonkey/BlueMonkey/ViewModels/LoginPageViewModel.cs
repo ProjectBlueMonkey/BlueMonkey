efda3b3ab19a1e24caf8f403adcd4860315c6e55
oota.kazuki <oota.kazuki@jp.fujitsu.com>
2016-12-13 10:10:04 +0900

    implement LoginPageViewModel

--- client/BlueMonkey/BlueMonkey/ViewModels/LoginPageViewModel.cs
+++ client/BlueMonkey/BlueMonkey/ViewModels/LoginPageViewModel.cs
@@ -1,13 +1,67 @@
-﻿using Prism.Mvvm;
+﻿using BlueMonkey.LoginService;
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using Prism.Services;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace BlueMonkey.ViewModels
 {
     public class LoginPageViewModel : BindableBase
     {
+        private INavigationService _navigationService;
+        private ILoginService _loginServic;
+        private IPageDialogService _pageDialogService;
+
+        public DelegateCommand LoginCommand { get; }
+
+        private bool _isBusy;
+        public bool IsBusy
+        {
+            get { return _isBusy; }
+            set { SetProperty(ref _isBusy, value); }
+        }
+
+        public LoginPageViewModel(INavigationService navigationService, ILoginService loginService, IPageDialogService pageDialogService)
+        {
+            _navigationService = navigationService;
+            _loginServic = loginService;
+            _pageDialogService = pageDialogService;
+
+            this.LoginCommand = new DelegateCommand(async () => await LoginAsync(), () => !IsBusy)
+                .ObservesProperty(() => IsBusy);
+        }
+
+        private async Task LoginAsync()
+        {
+            try
+            {
+                IsBusy = true;
+                var authUser = await _loginServic.LoginAsync();
+                if (authUser == null)
+                {
+                    await _pageDialogService.DisplayAlertAsync(
+                        "Information",
+                        "Login failed.",
+                        "OK");
+                    return;
+                }
+
+                await _navigationService.NavigateAsync("/NavigationPage/MainPage");
+            }
+            catch (Exception)
+            {
+                await _pageDialogService.DisplayAlertAsync(
+                    "Information",
+                    "Login failed.",
+                    "OK");
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+
+        }
     }
 }
