07d37a468254be39c2f1c62e1f7787c0eecc868d
oota.kazuki <oota.kazuki@jp.fujitsu.com>
2016-12-13 09:47:26 +0900

    add LoginService local stub

---
+++ client/BlueMonkey/BlueMonkey.LoginService.Local/LoginService.cs
@@ -0,0 +1,14 @@
+﻿using BlueMonkey.Business;
+using System;
+using System.Threading.Tasks;
+
+namespace BlueMonkey.LoginService.Local
+{
+    public class LoginService : ILoginService
+    {
+        public Task<AuthUser> LoginAsync()
+        {
+            return Task.FromResult(new AuthUser { Identity = Guid.NewGuid().ToString() });
+        }
+    }
+}
