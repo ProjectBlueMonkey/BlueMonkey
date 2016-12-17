5f676407003d70dda6e2410f023efdbcdf1f6c14
nuits.jp <nuits.jp@live.jp>
2016-12-17 16:37:27 +0900

    Add IMediaFile for MediaServices project.

---
+++ client/BlueMonkey/BlueMonkey.MediaServices/IMediaFile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueMonkey.MediaServices
+{
+    public interface IMediaFile : IDisposable
+    {
+        /// <summary>
+        /// Get stream if available
+        /// </summary>
+        /// <returns></returns>
+        Stream GetStream();
+    }
+}
