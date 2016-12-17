063434a564d39887ab0aca3b48ccc55ad95ce37d
nuits.jp <nuits.jp@live.jp>
2016-12-17 21:36:03 +0900

    Add Path property to IMediaFile.

--- client/BlueMonkey/BlueMonkey.MediaServices/IMediaFile.cs
+++ client/BlueMonkey/BlueMonkey.MediaServices/IMediaFile.cs
@@ -10,6 +10,10 @@ namespace BlueMonkey.MediaServices
     public interface IMediaFile : IDisposable
     {
         /// <summary>
+        /// Path to file.
+        /// </summary>
+        string Path { get; }
+        /// <summary>
         /// Get stream if available
         /// </summary>
         /// <returns></returns>
