6111c44fc342b09e2ddbbd19ec71afeab69c392b
nuits.jp <nuits.jp@live.jp>
2016-12-17 16:39:18 +0900

    Add IFileUploadService for FileUploadServices project.

---
+++ client/BlueMonkey/BlueMonkey.FileUploadServices/IFileUploadService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlueMonkey.MediaServices;
+
+namespace BlueMonkey.FileUploadServices
+{
+    public interface IFileUploadService
+    {
+        Task<Uri> UploadMediaFileAsync(IMediaFile mediaFile);
+    }
+}
