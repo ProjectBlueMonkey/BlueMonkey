4e449188dca626e34387c721255fa33f164639d0
nuits.jp <nuits.jp@live.jp>
2016-12-17 21:34:57 +0900

    Move IFileUploadService to BlueMonkey.ExpenseServices.

--- client/BlueMonkey/BlueMonkey.ExpenseServices/IFileUploadService.cs
+++ client/BlueMonkey/BlueMonkey.ExpenseServices/IFileUploadService.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using BlueMonkey.MediaServices;
 
-namespace BlueMonkey.FileUploadServices
+namespace BlueMonkey.ExpenseServices
 {
     public interface IFileUploadService
     {
