a729d18a6fe5a0151e1d587885308bd649f9a0c9
nuits.jp <nuits.jp@live.jp>
2016-12-17 22:59:54 +0900

    Add IEditExpense.

---
+++ client/BlueMonkey/BlueMonkey.Model/EditExpense.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlueMonkey.Business;
+using Prism.Mvvm;
+
+namespace BlueMonkey.Model
+{
+    public class EditExpense : BindableBase, IEditExpense
+    {
+        private Expense _expense;
+
+        public Expense Expense
+        {
+            get { return _expense; }
+            set { SetProperty(ref _expense, value); }
+        }
+        private IEnumerable<Category> _categories;
+        public IEnumerable<Category> Categories
+        {
+            get { return _categories; }
+            set { SetProperty(ref _categories, value); }
+        }
+    }
+}
---
+++ client/BlueMonkey/BlueMonkey.Model/IEditExpense.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlueMonkey.Business;
+
+namespace BlueMonkey.Model
+{
+    public interface IEditExpense : INotifyPropertyChanged
+    {
+        Expense Expense { get; }
+        IEnumerable<Category> Categories { get; }
+    }
+}
