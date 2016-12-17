9a64ebe5c6f8aff31f6a66bdd87ff0d0c8a95e15
nuits.jp <nuits.jp@live.jp>
2016-12-17 22:12:54 +0900

    Add Xam.Plugin.Media reference.

---
+++ client/BlueMonkey/BlueMonkey.MediaServices/MediaFile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueMonkey.MediaServices
+{
+    public class MediaFile : IMediaFile
+    {
+        public string Path { get; }
+        public Stream GetStream()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
