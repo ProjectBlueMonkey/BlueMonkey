5d827ea43a1b4d88e35ba5921ed5c624d0a35d52
nuits.jp <nuits.jp@live.jp>
2016-12-17 16:55:57 +0900

    Add IMediaService interface.

---
+++ client/BlueMonkey/BlueMonkey.MediaServices/StoreMediaOptions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueMonkey.MediaServices
+{
+    /// <summary>
+    /// Media Options
+    /// </summary>
+    public class StoreMediaOptions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        protected StoreMediaOptions()
+        {
+        }
+
+        /// <summary>
+        /// Directory name
+        /// </summary>
+        public string Directory
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// File name
+        /// </summary>
+        public string Name
+        {
+            get;
+            set;
+        }
+    }
+}
