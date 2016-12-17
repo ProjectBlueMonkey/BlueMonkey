1c4ad0ac3dc3a553626f08952303130c51de3160
Kazuki <k_ota28@hotmail.com>
2016-12-17 22:24:07 +0900

    add FileUploadService file

---
+++ client/BlueMonkey/BlueMonkey.ExpenseServices.Azure.Shared/AzureFileUploadService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using BlueMonkey.ExpenseServices;
+using BlueMonkey.MediaServices;
+
+namespace BlueMonkey.ExpenseServices.Azure
+{
+    public class AzureFileUploadService : IFileUploadService
+    {
+        public Task<Uri> UploadMediaFileAsync(IMediaFile mediaFile)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
