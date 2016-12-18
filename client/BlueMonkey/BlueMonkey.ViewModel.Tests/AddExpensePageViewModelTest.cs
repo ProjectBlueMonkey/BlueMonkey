f24c52e7c8c0754080b327b739fc9183228f787b
nuits.jp <nuits.jp@live.jp>
2016-12-19 08:45:42 +0900

    Implement UnitTest for AddExpensePageViewModel#Expense.

---
+++ client/BlueMonkey/BlueMonkey.ViewModel.Tests/AddExpensePageViewModelTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using BlueMonkey.Business;
+using BlueMonkey.Model;
+using BlueMonkey.ViewModels;
+using Moq;
+using Prism.Navigation;
+using Xunit;
+
+namespace BlueMonkey.ViewModel.Tests
+{
+    public class AddExpensePageViewModelTest
+    {
+        [Fact]
+        public void Expense()
+        {
+            var navigationService = new Mock<INavigationService>();
+            var editExpense = new Mock<IEditExpense>();
+
+            var actual = new AddExpensePageViewModel(navigationService.Object, editExpense.Object);
+
+            Assert.NotNull(actual.Expense);
+            Assert.Null(actual.Expense.Value);
+
+            var expense = new Expense();
+            editExpense.Setup(x => x.Expense).Returns(expense);
+            editExpense.Raise(m => m.PropertyChanged += null, new PropertyChangedEventArgs("Expense"));
+
+            Assert.Equal(expense, actual.Expense.Value);
+        }
+    }
+}
--- client/BlueMonkey/BlueMonkey/ViewModels/AddExpensePageViewModel.cs
+++ client/BlueMonkey/BlueMonkey/ViewModels/AddExpensePageViewModel.cs
@@ -6,37 +6,73 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reactive.Linq;
+using System.Threading.Tasks;
 using BlueMonkey.Model;
 using Reactive.Bindings;
 using Reactive.Bindings.Extensions;
 
 namespace BlueMonkey.ViewModels
 {
+    /// <summary>
+    /// ViewModel for AddExpensePage.
+    /// </summary>
     public class AddExpensePageViewModel : BindableBase, INavigationAware
     {
+        /// <summary>
+        /// INavigationService.
+        /// </summary>
         private readonly INavigationService _navigationService;
+
+        /// <summary>
+        /// EditExpense Use case model.
+        /// </summary>
         private readonly IEditExpense _editExpense;
 
+        /// <summary>
+        /// Target Expense
+        /// </summary>
         public ReadOnlyReactiveProperty<Expense> Expense { get; }
+
+        /// <summary>
+        /// Selectable categories.
+        /// </summary>
         public ReadOnlyReactiveProperty<IEnumerable<string>> Categories { get; }
 
+        /// <summary>
+        /// Selected Category.
+        /// </summary>
         public ReactiveProperty<string> SelectedCategory { get; }
 
-        public DelegateCommand SaveCommand => new DelegateCommand(Save);
-        public DelegateCommand NavigateCommand => new DelegateCommand(Navigate);
+        /// <summary>
+        /// command to save the expense.
+        /// </summary>
+        public AsyncReactiveCommand SaveAsyncCommand { get; }
 
+        /// <summary>
+        /// Command to navigate receipt page.
+        /// </summary>
+        public DelegateCommand NavigateReceiptPageCommand => new DelegateCommand(NavigateReceiptPage);
+
+        /// <summary>
+        /// Initialize instance.
+        /// </summary>
+        /// <param name="navigationService"></param>
+        /// <param name="editExpense"></param>
         public AddExpensePageViewModel(INavigationService navigationService, IEditExpense editExpense)
         {
             _navigationService = navigationService;
             _editExpense = editExpense;
 
             Expense = _editExpense.ObserveProperty(x => x.Expense).ToReadOnlyReactiveProperty();
+            // Convert, because picker supports only string.
             Categories = _editExpense.ObserveProperty(x => x.Categories)
                 .Where(x => x != null)
                 .Select(x => x.Select(category => category.Name)).ToReadOnlyReactiveProperty();
+            // Convert, because picker supports only string.
             SelectedCategory = _editExpense.ObserveProperty(x => x.SelectedCategory)
                 .Where(x => x != null)
                 .Select(x => x.Name).ToReactiveProperty();
+            // When you select into the Category name.
             SelectedCategory.Subscribe(x =>
             {
                 if (x == null)
@@ -48,28 +84,51 @@ namespace BlueMonkey.ViewModels
                     _editExpense.SelectedCategory = _editExpense.Categories.Single(category => category.Name == x);
                 }
             });
+
+            SaveAsyncCommand = new AsyncReactiveCommand();
+            SaveAsyncCommand.Subscribe(SaveAsync);
         }
 
-        private void Navigate()
+        /// <summary>
+        /// Navigation receipt page.
+        /// </summary>
+        private void NavigateReceiptPage()
         {
             _navigationService.NavigateAsync("ReceiptPage");
         }
 
-        private async void Save()
+        /// <summary>
+        /// Save edit expense.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <returns></returns>
+        private async Task SaveAsync(object sender)
         {
             await _editExpense.SaveAsync();
             await _navigationService.GoBackAsync();
         }
 
+        /// <summary>
+        /// Called when the implementer has been navigated away from.
+        /// </summary>
+        /// <param name="parameters"></param>
         public void OnNavigatedFrom(NavigationParameters parameters)
         {
 
         }
 
+        /// <summary>
+        /// Called when the implementer has been navigated to.
+        /// </summary>
+        /// <param name="parameters"></param>
         public void OnNavigatedTo(NavigationParameters parameters)
         {
         }
 
+        /// <summary>
+        /// Called before the implementor has been navigated to.
+        /// </summary>
+        /// <param name="parameters"></param>
         public async void OnNavigatingTo(NavigationParameters parameters)
         {
             await _editExpense.InitializeAsync();
