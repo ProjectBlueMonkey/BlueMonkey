096d78cef104e0c86c48065b348801ea9053b20e
nuits.jp <nuits.jp@live.jp>
2016-12-19 08:53:46 +0900

    Implement UnitTest for AddExpensePageViewModel#Categories.

--- client/BlueMonkey/BlueMonkey.ViewModel.Tests/AddExpensePageViewModelTest.cs
+++ client/BlueMonkey/BlueMonkey.ViewModel.Tests/AddExpensePageViewModelTest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
 using System.Linq;
+using System.Reactive.Linq;
 using System.Text;
 using BlueMonkey.Business;
 using BlueMonkey.Model;
@@ -31,6 +32,29 @@ namespace BlueMonkey.ViewModel.Tests
             editExpense.Raise(m => m.PropertyChanged += null, new PropertyChangedEventArgs("Expense"));
 
             Assert.Equal(expense, actual.Expense.Value);
+        }
+
+        [Fact]
+        public void Categories()
+        {
+            var navigationService = new Mock<INavigationService>();
+            var editExpense = new Mock<IEditExpense>();
+
+            var actual = new AddExpensePageViewModel(navigationService.Object, editExpense.Object);
+
+            Assert.NotNull(actual.Categories);
+            Assert.NotNull(actual.Categories.Value);
+            Assert.Equal(0, actual.Categories.Value.Count());
+
+            var categories = new [] { new Category {Name = "category1"}, new Category { Name = "category2" } };
+            editExpense.Setup(x => x.Categories).Returns(categories);
+            editExpense.Raise(m => m.PropertyChanged += null, new PropertyChangedEventArgs("Categories"));
+
+            var actualCategory = actual.Categories.Value?.ToList();
+            Assert.NotNull(actualCategory);
+            Assert.Equal(2, actualCategory.Count);
+            Assert.Equal("category1", actualCategory[0]);
+            Assert.Equal("category2", actualCategory[1]);
         }
     }
 }
