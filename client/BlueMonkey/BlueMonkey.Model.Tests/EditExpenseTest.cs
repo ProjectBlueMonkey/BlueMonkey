df564e19bc25e0304cc020942458c3c9076cd1e3
nuits.jp <nuits.jp@live.jp>
2016-12-19 14:03:21 +0900

    Add UnitTest for EditExpense#Name.

---
+++ client/BlueMonkey/BlueMonkey.Model.Tests/EditExpenseTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlueMonkey.ExpenseServices;
+using BlueMonkey.MediaServices;
+using BlueMonkey.TimeService;
+using Moq;
+using Xunit;
+
+namespace BlueMonkey.Model.Tests
+{
+    public class EditExpenseTest
+    {
+        [Fact]
+        public void NameProperty()
+        {
+            var expenseService = new Mock<IExpenseService>();
+            var fileUploadService = new Mock<IFileUploadService>();
+            var dateTimeService = new Mock<IDateTimeService>();
+            var mediaService = new Mock<IMediaService>();
+            var actual = new EditExpense(expenseService.Object, fileUploadService.Object, dateTimeService.Object, mediaService.Object);
+
+            Assert.Null(actual.Name);
+            Assert.PropertyChanged(actual, "Name", () => actual.Name = "update");
+            Assert.Equal("update", actual.Name);
+        }
+    }
+}
