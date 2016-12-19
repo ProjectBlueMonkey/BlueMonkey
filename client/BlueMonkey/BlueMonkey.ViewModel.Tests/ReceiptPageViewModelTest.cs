83f2a61eafb8bcb814f4bd3c6c16e91afd0e0eb4
nuits.jp <nuits.jp@live.jp>
2016-12-19 13:40:15 +0900

    Implement UnitTest for ReceiptPageViewModel#ReceiptProperty.

---
+++ client/BlueMonkey/BlueMonkey.ViewModel.Tests/ReceiptPageViewModelTest.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+using System.IO;
+using System.Windows.Media;
+using BlueMonkey.MediaServices;
+using BlueMonkey.Model;
+using BlueMonkey.ViewModels;
+using Moq;
+using Xunit;
+
+namespace BlueMonkey.ViewModel.Tests
+{
+    public class ReceiptPageViewModelTest
+    {
+        [Fact]
+        public void ReceiptProperty()
+        {
+            var editExpense = new Mock<IEditExpense>();
+            var actual = new ReceiptPageViewModel(editExpense.Object);
+
+            Assert.NotNull(actual.Receipt);
+            Assert.Null(actual.Receipt.Value);
+
+            var image = File.ReadAllBytes("lena.jpg");
+            editExpense.Setup(m => m.Receipt).Returns(new MediaFile(".jpg", image));
+            editExpense.Raise(m => m.PropertyChanged += null, new PropertyChangedEventArgs("Receipt"));
+
+            Assert.NotNull(actual.Receipt.Value);
+            // ImageSource doesn't expose any mechanism to retrieve the original byte array.
+            // For this reason, the test will give up by ImageSource.
+        }
+    }
+}
