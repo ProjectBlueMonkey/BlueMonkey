a6d4fa151fbd1a6f3cd0fb48fa280c44d5ffed3e
nuits.jp <nuits.jp@live.jp>
2016-12-21 17:27:38 +0900

    Fix Path to Extension for MediaFile.

---
+++ client/BlueMonkey/BlueMonkey.MediaServices/MediaServiceExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueMonkey.MediaServices
+{
+    public static class MediaFileExtensions
+    {
+        public static async Task<IMediaFile> Convert(this Plugin.Media.Abstractions.MediaFile source)
+        {
+            if (source != null)
+            {
+                using (var mediaStream = source.GetStream())
+                using (var memoryStream = new MemoryStream())
+                {
+                    await mediaStream.CopyToAsync(memoryStream);
+
+                    string extension;
+                    if (source.Path == null)
+                    {
+                        extension = null;
+                    }
+                    else
+                    {
+                        int index = source.Path.LastIndexOf('.');
+                        if (index < 0)
+                        {
+                            extension = null;
+                        }
+                        else
+                        {
+                            extension = source.Path.Substring(index, source.Path.Length - index);
+                        }
+                    }
+
+                    return new MediaFile(extension, memoryStream.ToArray());
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
