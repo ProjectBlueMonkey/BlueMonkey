780213011f50ac96d254e5657faec10295f912e5
nuits.jp <nuits.jp@live.jp>
2016-12-21 21:36:57 +0900

    Add ReferExpense#Constructor.

---
+++ client/BlueMonkey/BlueMonkey.Model.Tests/ReferExpenseTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlueMonkey.ExpenseServices;
+using Moq;
+using Xunit;
+
+namespace BlueMonkey.Model.Tests
+{
+    public class ReferExpenseTest
+    {
+        [Fact]
+        public void Constructor()
+        {
+            var expenseService = new Mock<IExpenseService>();
+            var actual = new ReferExpense(expenseService.Object);
+
+            Assert.NotNull(actual.Expenses);
+            Assert.Equal(0, actual.Expenses.Count);
+        }
+    }
+}
