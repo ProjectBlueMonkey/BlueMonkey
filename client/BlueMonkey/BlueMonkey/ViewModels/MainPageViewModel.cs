b2acd51b9d172d87749b38b0f2dc55e30405de7c
amay077 <okuokuoku@gmail.com>
2017-02-10 14:04:04 +0900

    Change to show alert if Secrets.cs is not edited

--- client/BlueMonkey/BlueMonkey/ViewModels/MainPageViewModel.cs
+++ client/BlueMonkey/BlueMonkey/ViewModels/MainPageViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Prism.Mvvm;
 using Prism.Navigation;
+using Prism.Services;
 using Reactive.Bindings;
 
 namespace BlueMonkey.ViewModels
@@ -15,6 +16,11 @@ namespace BlueMonkey.ViewModels
         /// </summary>
         private readonly INavigationService _navigationService;
 
+		/// <summary>
+		/// The page dialog service.
+		/// </summary>
+		private readonly IPageDialogService _pageDialogService;
+
         /// <summary>
         /// Command of navigate next pages.
         /// </summary>
@@ -24,9 +30,11 @@ namespace BlueMonkey.ViewModels
         /// Initialize instance.
         /// </summary>
         /// <param name="navigationService"></param>
-        public MainPageViewModel(INavigationService navigationService)
+		/// <param name="pageDialogService"></param>
+        public MainPageViewModel(INavigationService navigationService, IPageDialogService pageDialogService)
         {
             _navigationService = navigationService;
+			_pageDialogService = pageDialogService;
 
             NavigateCommand = new ReactiveCommand<string>();
             NavigateCommand.Subscribe(Navigate);
@@ -36,9 +44,18 @@ namespace BlueMonkey.ViewModels
         /// Navigate next page.
         /// </summary>
         /// <param name="uri"></param>
-        private void Navigate(string uri)
+		private async void Navigate(string uri)
         {
-            _navigationService.NavigateAsync(uri);
+			if (Secrets.ServerUri.Contains("your"))
+			{
+				await _pageDialogService.DisplayAlertAsync(
+					"Invalid Server Uri", 
+					"You should write actual ServerUri and ConnectionString to Secrets.cs",
+					"Close");
+				return;
+			}
+
+            await _navigationService.NavigateAsync(uri);
         }
     }
 }
