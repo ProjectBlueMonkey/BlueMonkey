ae2c43915a4877be3234fbdafbebd6113f912914
nuits.jp <nuits.jp@live.jp>
2017-04-12 14:02:15 +0900

    Remove Border of Entry (and other) for iOS

---
+++ client/BlueMonkey/BlueMonkey.Views/Controls/NoneBorderEntry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BlueMonkey.Views.Controls
+{
+    public class NoneBorderEntry : Entry
+    {
+    }
+}
